4a9fec5d7426ebdd0f069d23a1474dd13686ddec
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-14 18:44:58 +0100

    add javascript, update style

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,22 @@
-const mainElement = document.querySelector("main");
-
+const mainElement = document.querySelector("html");
+const cardElement = document.querySelector('[data-js="card-container"]');
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 const darkMode = document.querySelector('[data-js="dark-switch"]');
+const answerButton = document.querySelector('[data-js="answerButton"]');
+const answerText = document.querySelector('[data-js="hiddenAnswer"');
+
 darkMode.addEventListener("click", () => {
-  mainElement.classList.add("dark");
+  mainElement.classList.toggle("dark");
+});
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("active");
+});
+
+answerButton.addEventListener("click", () => {
+  if (answerText.hasAttribute("hidden")) {
+    answerText.removeAttribute("hidden");
+  } else {
+    answerText.setAttribute("hidden", "");
+  }
 });
