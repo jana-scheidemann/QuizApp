e3c0a440e3a31063d7fcf0ab4249b42044554125
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-14 20:25:06 +0100

    add form.html, update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,9 @@ const cardElement = document.querySelector('[data-js="card-container"]');
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
 const darkMode = document.querySelector('[data-js="dark-switch"]');
 const answerButton = document.querySelector('[data-js="answerButton"]');
-const answerText = document.querySelector('[data-js="hiddenAnswer"');
+const answerText = document.querySelector('[data-js="hiddenAnswer"]');
+const form = document.querySelector('[data-js="newCardForm"]');
+const formButton = document.querySelector('[data-js="formSubmitButton"]');
 
 darkMode.addEventListener("click", () => {
   mainElement.classList.toggle("dark");
@@ -14,9 +16,19 @@ bookmarkButton.addEventListener("click", () => {
 });
 
 answerButton.addEventListener("click", () => {
-  if (answerText.hasAttribute("hidden")) {
+  if (answerText.hidden === true) {
     answerText.removeAttribute("hidden");
   } else {
-    answerText.setAttribute("hidden", "");
+    answerText.setAttribute("hidden");
   }
 });
+
+form.addEventListener("submit", (event) => {});
+event.preventDefault();
+
+const formElements = event.target.elements;
+
+const formData = new FormData(event.target);
+const data = Object.fromEntries(formData);
+
+const newCard = document.createElement();
