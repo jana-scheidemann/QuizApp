42187d8f672de5e41dab0c97a9dcf026ecec7813
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-17 17:23:10 +0100

    fix buttons and dark-mode

--- JS/darkmode.js
+++
@@ -1,7 +0,0 @@
-// --- DARK MODE ---//
-const main_element = document.querySelector("html");
-const dark_mode = document.querySelector('[data-js="dark-switch"]');
-
-dark_mode.addEventListener("click", () => {
-  main_element.classList.toggle("dark");
-});
--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,33 @@
 // --- BOOKMARK BUTTON ---//
-const card = document.querySelector('[data-js="card"]');
-const bookmark_button = card.querySelector('[data-js="bookmark-button"]');
+const bookmark_buttons = document.querySelectorAll(
+  '[data-js="bookmark-buttons"]'
+);
 
-bookmark_button.addEventListener("click", () => {
-  bookmark_button.classList.toggle("bookmarkChecked");
+bookmark_buttons.forEach((bookmark_button) => {
+  bookmark_button.addEventListener("click", () => {
+    bookmark_button.classList.toggle("bookmarkChecked");
+  });
 });
 
 // --- ANSWER BUTTON ---//
-const answer_button = document.querySelector('[data-js="answer-button"]');
+const answer_buttons = document.querySelectorAll('[data-js="answer-button"]');
 const answer_text = document.querySelector('[data-js="answer-text"]');
+answer_buttons.forEach((answer_button) => {
+  answer_button.addEventListener("click", () => {
+    if (answer_text.hidden === true) {
+      answer_text.removeAttribute("hidden");
+      answer_button.textContent = "click to hide answer";
+    } else {
+      answer_text.setAttribute("hidden", "");
+      answer_button.textContent = "click to show answer";
+    }
+  });
+});
 
-answer_button.addEventListener("click", () => {
-  if (answer_text.hidden === true) {
-    answer_text.removeAttribute("hidden");
-    answer_button.textContent = "click to hide answer";
-  } else {
-    answer_text.setAttribute("hidden", "");
-    answer_button.textContent = "click to show answer";
-  }
+// --- DARK MODE ---//
+const html_element = document.querySelector("html");
+const dark_mode = document.querySelector('[data-js="dark-switch"]');
+
+dark_mode.addEventListener("click", () => {
+  html_element.classList.toggle("dark");
 });
